71dd23d68f534c9dd982bf96577174e3f762d694
R41Ryan <ryanhuynh64@gmail.com>
2022-08-30 21:04:18 -0600

    Implemented a function to calculate similarity
    The app is able to take in the contents of the two files, "testText1.txt" and "testText2.txt", and determine how similar they are with a value between 0.0 to 1.0 (0.0 being completely different, 1.0 being the complete same)

--- src/App.java
+++ src/App.java
@@ -1,7 +1,18 @@
 import userInterface.*;
 
+import java.io.File;
+import org.apache.pdfbox.text.*;
+import plagiarismAlgorithm.*;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
         UI main = new UI();
+        Checker checker = new Checker();
+
+        File file1 = new File("testText1.txt");
+        File file2 = new File("testText2.txt");
+
+        System.out.println(checker.getSimilarity(file1, file2));
     }
 }
--- src/plagiarismAlgorithm/checker.java
+++ src/plagiarismAlgorithm/checker.java
@@ -1,12 +1,89 @@
 package plagiarismAlgorithm;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Set;
+import java.lang.Math;
 
-public class checker {
-    public float getSimilarity(HashMap<String, Integer> wordFrequency1, HashMap<String, Integer> wordFrequency2)
+public class Checker {
+
+    public HashMap<String, Integer> getFreqencyMap(File file)
     {
+        HashMap<String, Integer> toReturn = new HashMap<String, Integer>();
+        Scanner scanner;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("Error occured while creating Scanner");
+            e.printStackTrace();
+            return null;
+        }
 
+        while (scanner.hasNext())
+        {
+            String word = scanner.next();
+            if (toReturn.containsKey(word))
+            {
+                toReturn.put(word, toReturn.get(word) + 1);
+            }
+            else
+            {
+                toReturn.put(word, 1);
+            }
+        }
 
-        return 0.0f;
-    };
+        scanner.close();
+
+        /*
+        Set<String> words = toReturn.keySet();
+        for (String string : words) {
+            System.out.printf("%s : %d\n", string, toReturn.get(string));
+        }
+        */
+
+        return toReturn;
+    }
+
+    public int dotProduct(HashMap<String, Integer> wordFrequency1, HashMap<String, Integer> wordFrequency2)
+    {
+        int sum = 0;
+
+        Set<String> keySet = wordFrequency1.keySet();
+        Iterator<String> keys = keySet.iterator();
+        while (keys.hasNext())
+        {
+            String key = keys.next();
+            //System.out.println(key);
+            if (wordFrequency1.containsKey(key) && wordFrequency2.containsKey(key))
+            {
+                // System.out.println("Both documents have this word");
+                sum += wordFrequency1.get(key) * wordFrequency2.get(key);
+            }
+            else
+            {
+                /*
+                System.out.println("Both documents do NOT have this word.");
+                */
+            }
+        }
+
+        return sum;
+    }
+
+    public double vectorAngle(HashMap<String, Integer> wordFrequency1, HashMap<String, Integer> wordFrequency2)
+    {
+        int numerator = dotProduct(wordFrequency1, wordFrequency2);
+        double denominator = Math.sqrt(dotProduct(wordFrequency1, wordFrequency1) * dotProduct(wordFrequency2, wordFrequency2));
+        return 1f - Math.acos(numerator/denominator) / (3.1416f / 2f);
+    }
+
+    public double getSimilarity(File file1, File file2)
+    {
+        HashMap<String, Integer> wordFrequencyMap1 = getFreqencyMap(file1);
+        HashMap<String, Integer> wordFrequencyMap2 = getFreqencyMap(file2);
+        return vectorAngle(wordFrequencyMap1, wordFrequencyMap2);
+    }
 }
