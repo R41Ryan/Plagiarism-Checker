1892452e072d276aa13b18e806454b8f7db29b89
R41Ryan <ryanhuynh64@gmail.com>
2022-10-19 19:56:53 -0600

    Added new test files and allowed the ability to select a folder, rather than just individual files.
    In addition, It's also been learned that pdf files with different fonts causes a bug within the similarity calculation function, resulting in a return of NaN. Oddly enough, this is only a bug in comparisons between two file that both have varying fonts; They have no problem being compared to the testPdf#.pdf files.

--- src/App.java
+++ src/App.java
@@ -1,9 +1,5 @@
 import userInterface.*;
 
-import java.io.File;
-import org.apache.pdfbox.text.*;
-import plagiarismAlgorithm.*;
-
 public class App {
 
     public static void main(String[] args) throws Exception {
--- src/userInterface/UI.java
+++ src/userInterface/UI.java
@@ -7,6 +7,7 @@ import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
+import java.util.List;
 import java.io.*;
 
 import plagiarismAlgorithm.*;
@@ -14,7 +15,7 @@ import plagiarismAlgorithm.*;
 public class UI implements ActionListener{
 
     // Data members used to return information
-    private File[] selectedFiles;
+    private List<File> selectedFiles;
     private double[][] fileSimilarities;
 
     // variables for various calculations
@@ -36,7 +37,7 @@ public class UI implements ActionListener{
     
     public UI() {
         // Initialize information data members
-        selectedFiles = new File[0];
+        selectedFiles = new ArrayList<File>();
 
         frame = new JFrame("Plagiarism Checker");
         frame.setVisible(true);
@@ -53,7 +54,8 @@ public class UI implements ActionListener{
 
         // Initialize JPanels *******************************************************************************
         panels.put("mainPage", new JPanel(new GridBagLayout()));
-        panels.put("fileSelection", new JPanel(new FlowLayout()));
+        panels.put("fileSelectionInner", new JPanel(new GridLayout(0, 2)));
+        panels.put("fileSelection", new JPanel(new GridBagLayout()));
         panels.put("fileSimilarity", new JPanel(new FlowLayout()));
 
         // Initialize Buttons *******************************************************************************
@@ -61,6 +63,8 @@ public class UI implements ActionListener{
         // Buttons for mainPage
         buttons.put("openFile", new JButton("Open File(s)"));
         buttons.get("openFile").addActionListener(this);
+        buttons.put("openFolder", new JButton("Open Folder"));
+        buttons.get("openFolder").addActionListener(this);
         buttons.put("checkPlagiarism", new JButton("Check Plagiarism"));
         buttons.get("checkPlagiarism").addActionListener(this);
         buttons.put("fileSelectionBack", new JButton("Back"));
@@ -91,6 +95,10 @@ public class UI implements ActionListener{
         scrollPanes.put("selectedFiles", new JScrollPane(textAreas.get("selectedFiles")));
         scrollPanes.get("selectedFiles").setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 
+        // scroll pane for file selection page
+        scrollPanes.put("fileSelection", new JScrollPane(panels.get("fileSelectionInner")));
+        scrollPanes.get("fileSelection").setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
         // scroll pane for file similarity page
         scrollPanes.put("fileSimilarity", new JScrollPane(textAreas.get("fileSimilarity")));
         scrollPanes.get("fileSimilarity").setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
@@ -114,6 +122,7 @@ public class UI implements ActionListener{
         panels.get("mainPage").removeAll();
 
         GridBagConstraints c = new GridBagConstraints();
+
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
         c.gridy = 0;
@@ -122,7 +131,14 @@ public class UI implements ActionListener{
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 1;
         c.gridy = 0;
+        panels.get("mainPage").add(buttons.get("openFolder"), c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 2;
+        c.gridwidth = 2;
         panels.get("mainPage").add(buttons.get("checkPlagiarism"), c);
+        c.gridwidth = 1;
 
         c.gridx = 0;
         c.gridy = 1;
@@ -132,12 +148,12 @@ public class UI implements ActionListener{
         c.gridy = 1;
         panels.get("mainPage").add(textFields.get("thresholdField"), c);
 
-        if (selectedFiles.length <= 0)
+        if (selectedFiles.size() <= 0)
         {
             textAreas.get("selectedFiles").setText(" Selected Files:\n");
         }
         c.gridx = 0;
-        c.gridy = 2;
+        c.gridy = 3;
         c.gridwidth = 2;
         panels.get("mainPage").add(scrollPanes.get("selectedFiles"), c);
 
@@ -148,11 +164,23 @@ public class UI implements ActionListener{
     void loadFileSelectionPage()
     {
         panels.get("fileSelection").removeAll();
+        panels.get("fileSelectionInner").removeAll();
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        panels.get("fileSelection").add(buttons.get("fileSelectionBack"), c);
+
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = 0;
+        c.gridy = 1;
         for (int i = 0; i < fileButtons.length; i++)
         {
-            panels.get("fileSelection").add(fileButtons[i]);
+            panels.get("fileSelectionInner").add(fileButtons[i]);
         }
-        panels.get("fileSelection").add(buttons.get("fileSelectionBack"));
+        panels.get("fileSelection").add(panels.get("fileSelectionInner"), c);
+
         frame.setContentPane(panels.get("fileSelection"));
     }
 
@@ -161,14 +189,14 @@ public class UI implements ActionListener{
     {
         panels.get("fileSimilarity").removeAll();
         textAreas.get("fileSimilarity").setText("");
-        for (int j = 0; j < selectedFiles.length; j++)
+        for (int j = 0; j < selectedFiles.size(); j++)
         {
-            String toPrint = "Similarity with " + selectedFiles[j].getName() + ":\n" + String.format("%.3f", fileSimilarities[i][j]) + "\n\n";
+            String toPrint = "Similarity with " + selectedFiles.get(j).getName() + ":\n" + String.format("%.3f", fileSimilarities[i][j]) + "\n\n";
             if (usingThreshold)
             {
                 if (fileSimilarities[i][j] >= thresholdValue)
                 {
-                    toPrint = "*** Similarity with " + selectedFiles[j].getName() + ": ***\n" + String.format("%.3f", fileSimilarities[i][j]) + "\n\n";
+                    toPrint = "*** Similarity with " + selectedFiles.get(j).getName() + ": ***\n" + String.format("%.3f", fileSimilarities[i][j]) + "\n\n";
                 }
             }
             textAreas.get("fileSimilarity").append(toPrint);
@@ -181,14 +209,14 @@ public class UI implements ActionListener{
     // Calculate the similarities for all files
     void calculateSimilarities()
     {
-        fileSimilarities = new double[selectedFiles.length][selectedFiles.length];
+        fileSimilarities = new double[selectedFiles.size()][selectedFiles.size()];
         Checker checker = new Checker();
-        for (int i = 0; i < selectedFiles.length; i++)
+        for (int i = 0; i < selectedFiles.size(); i++)
         {
-            for (int j = 0; j < selectedFiles.length; j++)
+            for (int j = 0; j < selectedFiles.size(); j++)
             {
                 try {
-                    fileSimilarities[i][j] = checker.getSimilarityPDF(selectedFiles[i], selectedFiles[j]);
+                    fileSimilarities[i][j] = checker.getSimilarityPDF(selectedFiles.get(i), selectedFiles.get(j));
                 } catch (IOException e) {
                     System.out.println("Error while calculating similarity\n");
                     e.printStackTrace();
@@ -201,11 +229,30 @@ public class UI implements ActionListener{
     // Create the buttons for the selected files
     void createFileButtons()
     {
-        fileButtons = new JButton[selectedFiles.length];
-        for (int i = 0; i < selectedFiles.length; i++)
+        fileButtons = new JButton[selectedFiles.size()];
+        for (int i = 0; i < selectedFiles.size(); i++)
         {
-            fileButtons[i] = new JButton(selectedFiles[i].getName());
+            fileButtons[i] = new JButton(selectedFiles.get(i).getName());
             fileButtons[i].addActionListener(this);
+        }
+    }
+
+    // Recursive function where it adds pdf files to selected files. If the file it's looking at is a directory, it calls itself
+    // Otherwise, it will add the file to the selected files
+    void addToSelectedPDF(File file)
+    {
+        for (File currentFile : file.listFiles())
+        {
+            if (currentFile.isDirectory())
+            {
+                System.out.println(currentFile.getName() + " is a folder. Calling recursively. *************************");
+                addToSelectedPDF(currentFile);
+            }
+            else if (currentFile.getName().endsWith(".pdf"))
+            {
+                System.out.println(currentFile.getName() + " is a file. Adding");
+                selectedFiles.add(currentFile);
+            }
         }
     }
 
@@ -214,7 +261,7 @@ public class UI implements ActionListener{
         int fileButtonSelected = -1;
         if (isInFileSelection)
         {
-            for (int i = 0; i < selectedFiles.length; i++)
+            for (int i = 0; i < selectedFiles.size(); i++)
             {
                 if (e.getSource() == fileButtons[i])
                 {
@@ -240,14 +287,43 @@ public class UI implements ActionListener{
 
             if (selectionReturn == JFileChooser.APPROVE_OPTION)
             {
-                selectedFiles = fileChooser.getSelectedFiles();
+                selectedFiles.clear();
+                for (File file : fileChooser.getSelectedFiles())
+                {
+                    selectedFiles.add(file);
+                }
 
                 int i = 0;
                 textAreas.get("selectedFiles").setText(" Selected Files:\n");
 
-                while (i < selectedFiles.length)
+                while (i < selectedFiles.size())
                 {
-                    textAreas.get("selectedFiles").append("\n     " + selectedFiles[i].getName());
+                    textAreas.get("selectedFiles").append("\n     " + selectedFiles.get(i).getName());
+                    i++;
+                }
+            }
+        }
+        else if (e.getSource() == buttons.get("openFolder"))
+        {
+            JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView());
+
+            fileChooser.setDialogTitle("Select a folder");
+            fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+            int selectionReturn = fileChooser.showOpenDialog(null);
+
+            if (selectionReturn == JFileChooser.APPROVE_OPTION)
+            {
+                selectedFiles.clear();
+                File directory = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                addToSelectedPDF(directory);;
+
+                int i = 0;
+                textAreas.get("selectedFiles").setText(" Selected Files:\n");
+
+                while (i < selectedFiles.size())
+                {
+                    textAreas.get("selectedFiles").append("\n     " + selectedFiles.get(i).getName());
                     i++;
                 }
             }
@@ -265,7 +341,7 @@ public class UI implements ActionListener{
                 System.out.println();
             }
             */
-            if (selectedFiles.length > 1)
+            if (selectedFiles.size() > 1)
             {
                 usingThreshold = checkboxes.get("thresholdValue").isSelected();
                 if (usingThreshold)
@@ -293,6 +369,7 @@ public class UI implements ActionListener{
             isInFileSelection = true;
         }
         
+        frame.pack();
         frame.validate();
     }
 }
